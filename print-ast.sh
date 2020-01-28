47d0c4c5fe8c82a1da711674d82074deedd69b0a
Brian Yee <brianyee@hotmail.com>
2020-01-27 21:49:46 -0700

    working

---
+++ print-ast.sh
@@ -0,0 +1,1 @@
+ ~/Downloads/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/bin/clang -cc1 -ast-dump $1
--- test.py
+++ test.py
@@ -9,7 +9,7 @@ def find_typerefs(node, typename):
     """ Find all references to the type named 'typename'
     """
     if node.kind.is_reference():
-        ref_node = clang.cindex.Cursor_ref(node)
+        ref_node = node.referenced 
         if ref_node.spelling == typename:
             print 'Found %s [line=%s, col=%s]' % (
                 typename, node.location.line, node.location.column)
@@ -17,6 +17,7 @@ def find_typerefs(node, typename):
     for c in node.get_children():
         find_typerefs(c, typename)
 
+clang.cindex.Config.set_library_path('/home/byee/Downloads/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/lib')
 index = clang.cindex.Index.create()
 tu = index.parse(sys.argv[1])
 print 'Translation unit:', tu.spelling
